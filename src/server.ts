edb29881db6810231225dad9d67d8bc16d7b77a2
Ayrton Souza <ayrtonsouza@icloud.com>
2021-08-24 17:19:45 -0300

    [:sparkles:] initial categories routes development

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ hello: 'world' });
-});
+app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(3333, () =>
   console.log('🚀 Server started and listening in: http://localhost:3333/')
