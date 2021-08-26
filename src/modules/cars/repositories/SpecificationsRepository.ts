d59ee501010d7c4640a99a72c5750b9cbd184aab
Ayrton Souza <ayrtonsouza@icloud.com>
2021-08-25 23:04:17 -0300

    [:hammer:] creating specifications resource

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,36 @@
+import { Specification } from '../model/Specification';
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from './ISpecificationsRepository';
+
+export class SpecificationsRepository implements ISpecificationsRepository {
+  private specifations: Specification[];
+
+  constructor() {
+    this.specifations = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifations.push(specification);
+  }
+
+  list(): Specification[] {
+    return this.specifations;
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifations.find(
+      (specification) => specification.name === name
+    );
+    return specification;
+  }
+}
