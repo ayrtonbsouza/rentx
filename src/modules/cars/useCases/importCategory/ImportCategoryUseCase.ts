94461caa78d2fdb703dea17bb775d62ce36654be
Ayrton Souza <ayrtonsouza@icloud.com>
2021-08-28 01:06:40 -0300

    [:construction:] saving imported categories to repository

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,16 +1,54 @@
 import csvParse from 'csv-parse';
 import fs from 'fs';
 
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 export class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
+  private categoriesRepository: ICategoriesRepository;
 
-    const parseFile = csvParse();
+  constructor(categoriesRepository: ICategoriesRepository) {
+    this.categoriesRepository = categoriesRepository;
+  }
 
-    stream.pipe(parseFile);
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+      const parseFile = csvParse();
+      stream.pipe(parseFile);
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', (error) => {
+          reject(error);
+        });
+    });
+  }
 
-    parseFile.on('data', async (line) => {
-      console.log(line);
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    categories.map(async (category) => {
+      const { name, description } = category;
+      const existentCategory = this.categoriesRepository.findByName(name);
+      if (!existentCategory) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
     });
   }
 }
