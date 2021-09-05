c6e47a5167ccfe0bed3c5b48f9ac806f0d4d0f19
Ayrton Souza <ayrtonsouza@icloud.com>
2021-09-05 01:58:58 -0300

    [:recycle:] encrypting password

--- src/modules/accounts/useCases/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
@@ -16,11 +17,16 @@ export class CreateUserUseCase {
     email,
     password,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+    if (userAlreadyExists) {
+      throw new Error('E-mail already in use');
+    }
+    const passwordHash = await hash(password, 8);
     await this.usersRepository.create({
       name,
       driver_license,
       email,
-      password,
+      password: passwordHash,
     });
   }
 }
