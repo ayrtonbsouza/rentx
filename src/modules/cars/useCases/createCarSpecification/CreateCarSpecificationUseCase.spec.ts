1fecd086d8151b2de2ff8663b289584a58baa333
Ayrton Souza <ayrtonsouza@icloud.com>
2021-09-27 21:58:30 -0300

    [:construction:] initial create cars specifications use case development

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -0,0 +1,40 @@
+import { AppError } from '@errors/AppError';
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+
+import { CreateCarSpecificationUseCase } from './CreateCarSpecificationUseCase';
+
+let createCarSpecification: CreateCarSpecificationUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe('Create Car Specification', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarSpecification = new CreateCarSpecificationUseCase(
+      carsRepositoryInMemory
+    );
+  });
+
+  it('should be able to add a new specification to a car', async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: 'Car Name',
+      description: 'Car Description',
+      daily_rate: 100,
+      license_plate: 'ABC1D23',
+      fine_amount: 50,
+      brand: 'Car Brand',
+      model: 'Car Model',
+      category_id: 'Car Category',
+    });
+
+    const specifications_id = ['0987654321'];
+    await createCarSpecification.execute({ car_id: car.id, specifications_id });
+  });
+
+  it('should not be able to add a new specification to a unexistent car', async () => {
+    expect(async () => {
+      const car_id = '1234567890';
+      const specifications_id = ['0987654321'];
+      await createCarSpecification.execute({ car_id, specifications_id });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -0,0 +1,19 @@
+import { inject } from 'tsyringe';
+
+import { AppError } from '@errors/AppError';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+
+interface IRequest {
+  car_id: string;
+  specifications_id: string[];
+}
+export class CreateCarSpecificationUseCase {
+  constructor(private carsRepository: ICarsRepository) {}
+  async execute({ car_id, specifications_id }: IRequest): Promise<void> {
+    const carExists = await this.carsRepository.findById(car_id);
+
+    if (!carExists) {
+      throw new AppError('Car does not exists');
+    }
+  }
+}
